f03ce2d6630ecf3a07b7e5aa39239acd2c7499cf
TayaUsacheva <116427632+TayaUsacheva@users.noreply.github.com>
2023-02-26 17:58:15 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,57 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+    bool flag = true;
+    for(int i = 2; i <= (value / 2); i++) {
+        if ((value % i) == 0)
+            flag = false;
+    }
+    return flag;
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    int number_prime = 0;
+    int x = 2;
+    while(number_prime < n) {
+        int kol_vo_del = 2;
+        for(int i = 2; i <= (x / 2); i++) {
+            if ((x % i) != 0)
+                kol_vo_del++;
+        }
+        if (kol_vo_del == 2) {
+            number_prime++;
+        }
+        x++;
+    }
+    return x;
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+    bool flag = false;
+    while(flag == false) {
+        value++;
+        for(int i = 2; i <= (value / 2); i++) {
+            if ((value % i) == 0)
+                flag = false;
+            else
+                flag = true;
+        }
+    }
+    return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+    int x = 2;
+    int sum = 2;
+    while((x +1) < hbound) {
+        x++;
+        int kol_vo_del = 2;
+        for(int i = 2; i <= (x / 2); i++) {
+            if ((x % i) == 0)
+                kol_vo_del++;
+        }
+        if(kol_vo_del == 2)
+            sum+=x;
+    }
+    return sum;
 }

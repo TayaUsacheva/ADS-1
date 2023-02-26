807a4734e13eb55edd977eb1f19af6046bdb2ec1
TayaUsacheva <116427632+TayaUsacheva@users.noreply.github.com>
2023-02-26 18:50:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,7 +5,7 @@
 
 bool checkPrime(uint64_t value) {
     bool flag = true;
-    for(int i = 2; i <= (value / 2); i++) {
+    for (int i = 2; i <= (value / 2); i++) {
         if ((value % i) == 0)
             flag = false;
     }
@@ -15,25 +15,22 @@ bool checkPrime(uint64_t value) {
 uint64_t nPrime(uint64_t n) {
     int number_prime = 0;
     int x = 2;
-    while(number_prime < n) {
-        int kol_vo_del = 2;
-        for(int i = 2; i <= (x / 2); i++) {
-            if ((x % i) != 0)
-                kol_vo_del++;
-        }
-        if (kol_vo_del == 2) {
+    int res = 0;
+    while (number_prime < n) {
+        if (checkPrime(x)) {
             number_prime++;
+            res = x;
         }
         x++;
     }
-    return x;
+    return res;
 }
 
 uint64_t nextPrime(uint64_t value) {
     bool flag = false;
-    while(flag == false) {
+    while (flag == false) {
         value++;
-        for(int i = 2; i <= (value / 2); i++) {
+        for (int i = 2; i <= (value / 2); i++) {
             if ((value % i) == 0)
                 flag = false;
             else
@@ -46,14 +43,14 @@ uint64_t nextPrime(uint64_t value) {
 uint64_t sumPrime(uint64_t hbound) {
     int x = 2;
     int sum = 2;
-    while((x +1) < hbound) {
+    while ((x +1) < hbound) {
         x++;
         int kol_vo_del = 2;
-        for(int i = 2; i <= (x / 2); i++) {
+        for (int i = 2; i <= (x / 2); i++) {
             if ((x % i) == 0)
                 kol_vo_del++;
         }
-        if(kol_vo_del == 2)
+        if (kol_vo_del == 2)
             sum+=x;
     }
     return sum;
